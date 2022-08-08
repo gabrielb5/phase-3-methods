a96ba790ea0bb683b856349cdbfba2daa9d5420e
Gabriel <gabrielburgosmm@gmail.com>
2022-08-08 12:16:42 -0600

    commit tree 5dc4b5c7bbecbbb2a9720dfea5eb485f421e2c0e

--- methods.rb
+++ methods.rb
@@ -1,1 +1,22 @@
 # Your code here!
+
+def greetProgrammer()
+    print "Hello, programmer!"
+end
+
+
+
+def greet name
+    puts "Hello, #{name}!"
+end
+
+def greetingWithDefault name = "programmer"
+    puts "Hello #{name}!"
+end
+
+def add num1, num2
+    num1 + num2
+end
+
+
+
